fa835357925ffaf3e566730e381b037c5028fb25
Muntakim-BJIT <88704632+Muntakim-BJIT@users.noreply.github.com>
2021-08-30 20:18:04 +0600

    Added directive for dropdowns

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { Ingredient } from 'src/app/shared/ingredient.model';
+import { Ingredient } from 'src/app/shared/models/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
